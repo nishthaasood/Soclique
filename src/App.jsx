ab0561daa6428eb1c03736a5e53eb59a826ba51e
nishthaasood <soodnishtha12462@gmail.com>
2025-09-09 01:00:14 +0530

    Resolved issues

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,49 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Navbar.jsx'
+import GetStarted from './Get-started.jsx' 
 
 function App() {
   const [currentPage, setCurrentPage] = useState('home')
   const [accessToken, setAccessToken] = useState(null)
 
+  const renderCurrentPage = () => {
+    switch(currentPage) {
+      case 'home':
+        return (
+          <div>
+            <h1>Home Page</h1>
+            <p>Welcome to the home page!</p>
+          </div>
+        );
+      case 'about':
+        return (
+          <div>
+            <h1>About Us</h1>
+            <p>Learn more about us here.</p>
+          </div>
+        );
+      case 'get-started':
+        return <GetStarted />;
+      default:
+        return (
+          <div>
+            <h1>Home Page</h1>
+            <p>Welcome to the home page!</p>
+          </div>
+        );
+    }
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar 
+        setCurrentPage={setCurrentPage} 
+        currentPage={currentPage} 
+      />
+      <main>
+        {renderCurrentPage()}
+      </main>
     </div>
   );
 }
